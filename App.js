93ca1ba1e731dcd7cb6fd9688b489451e00c3596
blame-zeus <jjzeuus@gmail.com>
2024-10-16 23:51:39 +0800

    This is my work

---
+++ App.js
@@ -0,0 +1,63 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import MyActivityIndicator from './codes/MyActivityIndicator';
+import MyButton from './codes/MyButton';
+//import MyFlatList from './codes/MyFlatList';
+import MyImage from './codes/MyImage';
+import MyImageBackground from './codes/MyImageBackground';
+import MyKeyboardAvoidingView from './codes/MyKeyboardAvoidingView';
+import MyModal from './codes/MyModal';
+import MyPressable from './codes/MyPressable';
+import MyRefreshControl from './codes/MyRefreshControl';
+import MyScrollView from './codes/MyScrollView';
+import MySectionList from './codes/SectionList';
+import MyStatusBar from './codes/StatusBar';
+import MySwitch from './codes/MySwitch';
+import MyText from './codes/MyText';
+import MyTextInput from './codes/MyTextInput';
+import MyTouchableHighlight from './codes/MyTouchableHighlight';
+import MyTouchableOpacity from './codes/MyTouchableOpacity';
+import MyTouchableWithoutFeedback from './codes/MyTouchableWithoutFeedback';
+import MyView from './codes/MyView';
+import MyVirtualizedList from './codes/MyVirtualizedList';
+
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      {/* <MyActivityIndicator/> */}
+      {/* <MyButton/> */}
+      {/* <MyFlatList/> */}
+      {/* <MyImage/> */}
+      {/* <MyImageBackground/> */}
+      {/* <MyKeyboardAvoidingView/> */}
+      {/* <MyModal/> */}
+      {/* <MyPressable/> */}
+      {/* <MyRefreshControl/> */}
+      {/* <MyScrollView/> */}
+      {/* <MySectionList/> */}
+      {/* <MyStatusBar/> */}
+      {/* <MySwitch/> */}
+      {/* <MyText/> */}
+      {/* <MyTextInput/> */}
+      {/* <MyTouchableHighlight/> */}
+      {/* <MyTouchableOpacity/> */}
+      {/* <MyTouchableWithoutFeedback/> */}
+      {/* <MyView/> */}
+      {/* <MyVirtualizedList/> */}
+
+
+
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
